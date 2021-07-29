2b3e2c8b1d691a9d99e91cec97ef76d0de125e02
kunal1823007 <kunal.raju2307@outlook.com>
2021-07-29 22:56:38 +0530

    first

--- src/main/java/com/springboot/findo_project/homecontroller.java
+++ src/main/java/com/springboot/findo_project/homecontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class homecontroller {
 	
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String home() {
 		
 		return "home";
