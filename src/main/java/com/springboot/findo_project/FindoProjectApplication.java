3e5e4fc0a69eb36a9ba55a378d470f6af5b6e8ac
kunal1823007 <kunal.raju2307@outlook.com>
2021-07-29 23:43:27 +0530

    first

--- src/main/java/com/springboot/findo_project/FindoProjectApplication.java
+++ src/main/java/com/springboot/findo_project/FindoProjectApplication.java
@@ -2,8 +2,10 @@ package com.springboot.findo_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class FindoProjectApplication {
 
 	public static void main(String[] args) {
