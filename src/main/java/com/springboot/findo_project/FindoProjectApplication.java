f125ef49eb5a71e78e1f4274d500006c94cfea08
kunal1823007 <kunal.raju2307@outlook.com>
2021-07-29 23:32:38 +0530

    first

--- src/main/java/com/springboot/findo_project/FindoProjectApplication.java
+++ src/main/java/com/springboot/findo_project/FindoProjectApplication.java
@@ -2,10 +2,8 @@ package com.springboot.findo_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FindoProjectApplication {
 
 	public static void main(String[] args) {
