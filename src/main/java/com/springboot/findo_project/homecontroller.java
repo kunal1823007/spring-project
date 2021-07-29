746cbcc761289aaca526e0c02fd5764961d6cdcd
kunal1823007 <kunal.raju2307@outlook.com>
2021-07-29 22:32:43 +0530

    first

--- src/main/java/com/springboot/findo_project/homecontroller.java
+++ src/main/java/com/springboot/findo_project/homecontroller.java
@@ -2,10 +2,8 @@ package com.springboot.findo_project;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RestController
 public class homecontroller {
 	
 	@RequestMapping("/")
