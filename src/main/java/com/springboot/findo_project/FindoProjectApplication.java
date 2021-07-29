c5c80e2c41be8873850e983e5298f185d74e8509
kunal1823007 <kunal.raju2307@outlook.com>
2021-07-29 14:20:50 +0530

    first

--- src/main/java/com/springboot/findo_project/FindoProjectApplication.java
+++ src/main/java/com/springboot/findo_project/FindoProjectApplication.java
@@ -2,17 +2,9 @@ package com.springboot.findo_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class FindoProjectApplication {
-	@RequestMapping("/")
-	public String home() {
-		
-		return "home";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FindoProjectApplication.class, args);
